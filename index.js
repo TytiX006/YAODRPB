fd024e33e04f88f534d035a642d0a37fafbf13db
TytiX006 <developper.belliard@gmail.com>
2019-01-10 13:22:16 +0100

    init bot

---
+++ index.js
@@ -0,0 +1,50 @@
+var Discord = require('discord.io');
+
+var app = require('express')();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.send('hello');
+});
+
+app.listen(PORT, function () {
+  console.log('server started on PORT', PORT);
+});
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: process.env.DISCORD_TOKEN,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
