62492532d62a7b1fb0b0dc6871d7aa36b2586216
TytiX006 <developper.belliard@gmail.com>
2019-01-10 13:52:16 +0100

    Change discord lib

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Discord = require('discord.io');
+// var Discord = require('discord.io');
 
 var app = require('express')();
 
@@ -15,36 +15,62 @@ app.listen(PORT, function () {
 
 console.log('TOKEN ------------', process.env.DISCORD_TOKEN);
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: process.env.DISCORD_TOKEN,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    console.log('Connected');
-    console.log('Logged in as: ');
-    console.log(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+// // Initialize Discord Bot
+// var bot = new Discord.Client({
+//    token: process.env.DISCORD_TOKEN,
+//    autorun: true
+// });
+// bot.on('ready', function (evt) {
+//     console.log('Connected');
+//     console.log('Logged in as: ');
+//     console.log(bot.username + ' - (' + bot.id + ')');
+// });
+// bot.on('message', function (user, userID, channelID, message, evt) {
+//     // Our bot needs to know if it will execute a command
+//     // It will listen for messages that will start with `!`
+//     if (message.substring(0, 1) == '!') {
+//         var args = message.substring(1).split(' ');
+//         var cmd = args[0];
+//
+//         args = args.splice(1);
+//         switch(cmd) {
+//             // !ping
+//             case 'ping':
+//                 bot.sendMessage({
+//                     to: channelID,
+//                     message: 'Pong!'
+//                 });
+//             break;
+//             // Just add any case commands if you want to..
+//          }
+//      }
+// });
+// bot.on('error', function(err) {
+//   console.error(err);
+// });
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
 });
-bot.on('error', function(err) {
-  console.error(err);
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
 });
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(process.env.DISCORD_TOKEN);
